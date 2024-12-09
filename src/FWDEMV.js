722208e3e421441577f7a0ee55cc1bd0488bf3b7
vineet <vineet@digitizeco.in>
2024-12-09 15:23:59 +0530

    Updates

--- src/FWDEMV.js
+++ src/FWDEMV.js
@@ -48838,8 +48838,8 @@ const _FWDEMV = class _FWDEMV extends FWDEMVEventDispather {
       this.width = this.stageContainer.offsetWidth;
       if (this.autoScale && this.width <= this.startResizingWidth) {
         this.height = Math.round(this.maxHeight * (this.width / this.startResizingWidth));
-        if (this.height < 360)
-          this.height = 360;
+        if (this.height < 500)
+          this.height = 500;
       } else {
         this.height = this.maxHeight;
       }
